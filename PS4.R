37e3067189c9c268b7e151543b76ad72a1b6e2c3
Mauricio Vela <mauricioandresvela@gmail.com>
2016-03-02 12:28:33 -0500

    1st

--- PS4.R
+++ PS4.R
@@ -1,10 +1,25 @@
-library(rvest)    
+library(rvest) 
+library(XML)
 wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_elections_by_popular_vote_margin'
 
 ## Grab the tables from the page and use the html_table function to extract the tables.
 ## You need to subset temp to find the data you're interested in (HINT: html_table())
 
-temp <- wikiURL %>% 
-  read_html %>%
-  html_nodes("table")
 
+
+table <- wikiURL %>%  
+  read_html %>% 
+  html_nodes("table")  %>% 
+  .[[2]] %>% 
+  html_table(trim=TRUE)
+
+
+#fix column names 
+colnames(table)[6] <- "percmargin"
+colnames(table)[8] <- "votemargin"
+
+#remove first two rows (only with names)
+table <- table[-c(1,2),]
+
+#fix margin variables
+table$percmargin <- gsub(,table$percmargin)
